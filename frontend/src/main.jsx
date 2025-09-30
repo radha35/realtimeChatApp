93186cfe61be57bdb32c745c2160ee30f8373d7a
Radha Chittalwar <117638869+radha35@users.noreply.github.com>
2025-10-01 01:27:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://realtimechatapp-backend-v2lp.onrender.com"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Provider store={store}>
